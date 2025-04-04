fccd45d9997002e358c742fb5ce741626aa633fa
Carlos Aldaez <carlos.aldaez@gmail.com>
2025-04-04 12:52:59 -0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,36 @@
+/*
+=======================================================================
+Crear la Base de datos y esquemas
+=======================================================================
+Script:
+  Este script crea una nueva base de datos 'DataWarehouse' después de verificar si ya existe.
+  Si existe la base de datos, se elimina y se crea nuevamente. Así mismo crea tres esquemas en la base de datos: 'bronce', 'plata' y 'oro'.
+/*
+
+USE MASTER;
+GO
+
+-- Eliminar y recrear la base de datos 'DataWarehouse'
+IF EXISTS (SELECT 1 FROM sys.databases WHWRE name = 'DataWarehouse')
+BEGIN
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK INMEDIATE
+  DROP DATABASE DataWarehouse;
+END;
+GO
+
+--Crear la base de datos 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+--Crear esquemas
+CREATE SCHEMA bronce;
+GO
+
+CREATE SCHEMA plata;
+GO
+
+CREATE SCHEMA oro;
+GO
