5d16eeb32b396631996bf717bb1e7e8e493fe5f5
Carlos Aldaez <carlos.aldaez@gmail.com>
2025-04-07 21:50:22 -0500

    Update ddl_bronce.sql

--- scripts/bronce/ddl_bronce.sql
+++ scripts/bronce/ddl_bronce.sql
@@ -8,7 +8,7 @@ Objetivo del Script:
 =============================================================================================
 */
 
-IF OBJECT ID ('bronce.crm_cust_inf', 'u') IS NOT NULL
+IF OBJECT_ID ('bronce.crm_cust_inf', 'u') IS NOT NULL
   DROP TABLE bronce.crm_cust_inf;
 CREATE TABLE bronce.crm_cust_inf (
   cst_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE bronce.crm_cust_inf (
   cst_create_date DATE
 );
 
-IF OBJECT ID ('bronce.crm_prd_inf', 'u') IS NOT NULL
+IF OBJECT_ID ('bronce.crm_prd_inf', 'u') IS NOT NULL
   DROP TABLE bronce.crm_prd_inf;
 CREATE TABLE bronce.crm_prd_inf (
   prd_id INT,
@@ -32,7 +32,7 @@ CREATE TABLE bronce.crm_prd_inf (
   prd_end_dt DATETIME
 );
 
-IF OBJECT ID ('bronce.crm_sales_details', 'u') IS NOT NULL
+IF OBJECT_ID ('bronce.crm_sales_details', 'u') IS NOT NULL
   DROP TABLE bronce.crm_sales_details;
 CREATE TABLE bronce.crm_sales_details (
   sls_ord_num NVARCHAR(50),
@@ -46,7 +46,7 @@ CREATE TABLE bronce.crm_sales_details (
   sls_price FLOAT
 );
 
-IF OBJECT ID ('bronce.erp_cust_az12', 'u') IS NOT NULL
+IF OBJECT_ID ('bronce.erp_cust_az12', 'u') IS NOT NULL
   DROP TABLE bronce.erp_cust_az12;
 CREATE TABLE bronce.erp_cust_az12 (
   cid NVARCHAR(50),
@@ -54,14 +54,14 @@ CREATE TABLE bronce.erp_cust_az12 (
   gen NVARCHAR(50)
 );
 
-IF OBJECT ID ('bronce.erp_loc_a101', 'u') IS NOT NULL
+IF OBJECT_ID ('bronce.erp_loc_a101', 'u') IS NOT NULL
   DROP TABLE bronce.erp_loc_a101 ;
 CREATE TABLE bronce.erp_loc_a101 (
   cid NVARCHAR(50),
   cntry NVARCHAR(50)
 );
 
-IF OBJECT ID ('bronce.erp_px_cat_g1v2', 'u') IS NOT NULL
+IF OBJECT_ID ('bronce.erp_px_cat_g1v2', 'u') IS NOT NULL
   DROP TABLE bronce.erp_px_cat_g1v2;
 CREATE TABLE bronce.erp_px_cat_g1v2 (
   id NVARCHAR(50),
